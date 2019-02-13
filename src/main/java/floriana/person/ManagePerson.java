34ac1229dbd6e5adb3f1279d7a6f4da33ed6b95f
florian.anghele <florian.anghele@P5308.ro.softvision.global>
2019-02-13 17:31:23 +0200

    Am facut commit

--- src/main/java/floriana/person/ManagePerson.java
+++ src/main/java/floriana/person/ManagePerson.java
@@ -1,25 +1,22 @@
 package floriana.person;
 
-import java.sql.SQLOutput;
+import floriana.student.Student;
 
 public class ManagePerson {
 
     public static void main(String[] args) {
 
-        Person p1 = new Person("Florian", "Anghele");
+    Person s1 = new Student("Florian","Anghele",true,12);
 
-        System.out.println(p1);
+        System.out.println(s1);
 
-        p1.setGender("Male");
-        System.out.println("Si este " + p1.getGender() + ".");
+        ((Student) s1).addDisciplineMark("Java",10);
 
-        p1.setEmailAddress("florian@gmail.com");
-        System.out.println("Si are email-ul: " + p1.getEmailAddress());
 
-        p1.setAge(18);
-        System.out.println("Si are " + p1.getAge() + " de ani.");
-        p1.setEmailRestricted(true);
+        ((Student) s1).addDisciplineMark("Php",12);
 
-        System.out.println(p1.printPersonDetails());
+        System.out.println("Average is: " + ((Student) s1).calculateAverageDisciplinesScore());
+
+        System.out.println(((Student) s1).getDisciplineMarks());
     }
 }
