4ae6b2d94d17163b1dabdecd715486af9bf5f5ed
Alex Caciur <alexandru.caciur@P5808.ro.softvision.global>
2019-02-13 14:11:48 +0200

    ManagePerson.java

---
+++ src/main/java/alexc/person/ManagePerson.java
@@ -0,0 +1,23 @@
+package alexc.person;
+
+import static java.lang.Boolean.*;
+
+public class ManagePerson {
+
+    public static void main(String[] args) {
+        Person alex=new Person();
+        alex.setFirstName("Alex");
+        alex.setLastName("Caciur");
+        alex.setAge(23);
+        alex.setGender("Male");
+        alex.setEmailAddress("caciuralex@gmail.com");
+        alex.setEmailRestricted(TRUE);
+
+        //System.out.println("Numele persoanei este: " + alex.getLastName() + " " + alex.getFirstName());
+        //System.out.println("Varsta: "+alex.getAge()+" Gender: "+alex.getGender());
+        //System.out.println("Emailul este: "+alex.getEmailAddress());
+
+        alex.getInfo();
+    }
+
+}
