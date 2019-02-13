f9899ba1796029d49e89966a76144e833f1cb0c4
Chirila Cosmina Nicoleta <cosmina.chirila@P5946.ro.softvision.global>
2019-02-13 17:32:47 +0200

    Created another class + modified the other two

--- src/main/java/cosminac/person/ManagePerson.java
+++ src/main/java/cosminac/person/ManagePerson.java
@@ -1,9 +1,11 @@
 package cosminac.person;
 
+import students.Student;
+
 public class ManagePerson {
 
     public static void main(String[] args) {
-        Person cosminaPerson = new Person("Cosmina", "Chirila", "Female", "something@something.com", true);
+        Person cosminaPerson = new Person("Cosmina", "Chirila", "Female", "something@something.com", false);
         System.out.println("The person name is " + cosminaPerson.getFirstName() + cosminaPerson.getLastName());
 
         cosminaPerson.setGender("Female");
@@ -15,7 +17,14 @@ public class ManagePerson {
 
 //        cosminaPerson.isEmailRestricted();
 
+        Student cosmina = new Student( "Cosmina", "Chirila", true, 1);
+        cosmina.addDisciplineMark("Info", 10);
+        cosmina.addDisciplineMark("Biology", 6);
+        cosmina.addDisciplineMark("Geography", 5);
+        System.out.println(cosmina.getDisciplineMarks());
+        System.out.println(cosmina.calculateAverageDisciplinesScore());
     }
+
 }
 
 
