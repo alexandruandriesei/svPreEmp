6b0c0b75ec9b0d3d79a88b91691321e2b44ce7e7
unknown <anamaria.buruiana@SVROLP01010.ro.softvision.global>
2019-12-05 15:19:08 +0200

    Created new class Person Updates done after review: FirstName ->firstName and LastName->lastName

---
+++ src/main/java/anab/Person.java
@@ -0,0 +1,46 @@
+public class Person {
+
+  String FirstName;
+    public String getFirstName() {
+        return FirstName;
+    }
+    public void setFirstName(String newFirstName) {
+        FirstName = newFirstName;
+    }
+
+
+    String LastName;
+    public String getLastName() {
+        return LastName;
+    }
+    public void setLastName(String newLastName) {
+        LastName = newLastName;
+    }
+
+
+    String Age;
+    public String getAge() {
+        return Age;
+    }
+    public void setAge(String newAge) {
+        Age = newAge;
+    }
+
+
+    String Gender;
+    public String getGender() {
+        return Gender;
+    }
+    public void setGender(String newGender) {
+        Gender = newGender;
+    }
+
+
+    String EmailAddress;
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+    public void setEmailAddress(String newEmailAddress) {
+        EmailAddress = newEmailAddress;
+    }
+}
