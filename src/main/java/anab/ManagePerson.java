2ceef6c397e4452f3f1a1807d0aa7689a666f4bb
anamaria.buruiana <anamaria.buruiana@softvision.ro>
2019-12-09 10:11:51 +0200

    Updates on variables name from UpperCase to LowerCase and added ManagePerson class.

--- src/main/java/anab/ManagePerson.java
+++ src/main/java/anab/ManagePerson.java
@@ -2,11 +2,11 @@ package anab;
 
 public class ManagePerson {
 
-        private String firstName;
-        private String lastName;
-        private int age;
-        private char gender;
-        private char email_address;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private char gender;
+    private char email_address;
 
     public ManagePerson(String firstName, String lastName, int age, char gender, char email_address) {
         this.firstName = firstName;
--- src/main/java/anab/Person.java
+++ src/main/java/anab/Person.java
@@ -2,46 +2,47 @@ package anab;
 
 public class Person {
 
-  String firstName;
+    String firstName, lastName, Age, Gender, EmailAddress;
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String newFirstName) {
         firstName = newFirstName;
     }
 
 
-    String lastName;
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String newLastName) {
         lastName = newLastName;
     }
 
 
-    String Age;
     public String getAge() {
         return Age;
     }
+
     public void setAge(String newAge) {
         Age = newAge;
     }
 
 
-    String Gender;
     public String getGender() {
         return Gender;
     }
+
     public void setGender(String newGender) {
         Gender = newGender;
     }
 
-
-    String EmailAddress;
     public String getEmailAddress() {
         return EmailAddress;
     }
+
     public void setEmailAddress(String newEmailAddress) {
         EmailAddress = newEmailAddress;
     }
